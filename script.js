92ff86a42bcb503f7ddcf47aec530bd0bc0c202c
Faryal407 <faryal77779@gmail.com>
2025-11-30 23:00:12 +0500

    add initial files

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// Add to Cart Functions
+
+let orderBtns = document.querySelectorAll("#orderBtn");
+
+orderBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Your order has been added! ☕");
+  });
+});
+
+// Navbar Shadow on Scroll
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    navbar.classList.add("shadow-lg", "bg-brown/80", "backdrop-blur-md");
+  } else {
+    navbar.classList.remove("shadow-lg", "bg-brown/80", "backdrop-blur-md");
+  }
+});
+
+
+
+
+// Dark / Light Mode Toggle (Optional)
+
+let modeBtn = document.querySelector("#modeBtn");
+        modeBtn.addEventListener("click", () => {
+            document.body.style.backgroundColor = "lightpink"; // Dark background
+        });
